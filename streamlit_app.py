ea084177a25828cf7f7ee2d2309af6a479b5f99a
TSnowflake <126820865+TSnowflake@users.noreply.github.com>
2023-03-09 11:47:28 +0100

    Update streamlit_app.py
    Normalized json format and make it as a table in the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize json fromat 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write it as a table
+streamlit.dataframe(fruityvice_normalized)
