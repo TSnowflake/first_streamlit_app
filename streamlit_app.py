eec5b3c7b6a1950fd2c5fce98849d5e9c65c14a2
TSnowflake <126820865+TSnowflake@users.noreply.github.com>
2023-03-29 11:59:05 +0200

    Update streamlit_app.py
    added another text box for choosing fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+# Creating another text box 
+fruits_selected = streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+# Display the table on the page.
+streamlit.dataframe (fruits_to_show)
