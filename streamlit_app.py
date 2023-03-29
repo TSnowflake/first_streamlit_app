3a823976440e26f1417f18de083b902f3d4f20f9
TSnowflake <126820865+TSnowflake@users.noreply.github.com>
2023-03-29 12:28:41 +0200

    Update streamlit_app.py
    added try-except and if-else

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,19 +23,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe (fruits_to_show)
 
+#New section with try-except (with if-else inside)
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error ("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error ()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# write it as a table
 streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
-
 # normalize json fromat 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write it as a table
-streamlit.dataframe(fruityvice_normalized)
+
 #Don't run anything past here while we troubleshoot
 streamlit.stop()
 
